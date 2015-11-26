aec2d5d81ecba1e40ccf83f310eba1371902faa2
Martin Janser <martin@duss-janser.ch>
2015-11-26 16:14:36 +0100

    Fix idempotence check in test script

--- tests/test.sh
+++ tests/test.sh
@@ -5,6 +5,6 @@ ansible-playbook playbook.yml --syntax-check || exit 1
 ansible-playbook playbook.yml --connection=local || exit 1
 
 ansible-playbook playbook.yml --connection=local | tee /tmp/idempotence.log
-grep -q "changed=0.*failed=0" /tmp/idempotence.log \
+sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" /tmp/idempotence.log | grep -q "changed=0.*failed=0" \
     && (echo "Idempotence test: pass" && exit 0) \
     || (echo "Idempotence test: fail" && exit 1)
